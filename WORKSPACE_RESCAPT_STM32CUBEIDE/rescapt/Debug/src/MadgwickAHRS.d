b1cd72be9fa1be569ddee8c8204c9e2800d85bc5
Eljily-Mohamed <hamadekj15@gmail.com>
2024-10-16 10:10:37 +0200

    end

---
+++ WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/Debug/src/MadgwickAHRS.d
@@ -0,0 +1,3 @@
+src/MadgwickAHRS.o: ../src/MadgwickAHRS.c \
+ /home/mohamed/enib/s8/RES/RES_CAN/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/inc/MadgwickAHRS.h
+/home/mohamed/enib/s8/RES/RES_CAN/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/inc/MadgwickAHRS.h:
